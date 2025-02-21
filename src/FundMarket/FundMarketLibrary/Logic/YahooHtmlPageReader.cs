8c3825130803a3a4057e8aee92d21a21c64f4e46
Dmytro Trofimchuk <dmytro.trofimchuk@acupower.co.uk>
2025-02-21 17:23:36 +0200

    feature/1

--- src/FundMarket/FundMarketLibrary/Logic/YahooHtmlPageReader.cs
+++ src/FundMarket/FundMarketLibrary/Logic/YahooHtmlPageReader.cs
@@ -5,25 +5,35 @@ namespace FundMarketLibrary.Logic;
 
 public class YahooHtmlPageReader : IAssetReader
 {
-    const string YahooUrl = "https://finance.yahoo.com/";
+    private const string YahooUrl = "https://finance.yahoo.com/";
 
     public Asset GetAsset(string ticker)
     {
         var defUrl = $"{YahooUrl}quote/{ticker}";
         var analysisUrl = $"{defUrl}/analysis";
-
         var web = new HtmlWeb();
         var analysisDoc = web.Load(analysisUrl);
         var futurePrice = GetFirstElementInnerText(analysisDoc, "//div[contains(@class, 'average')]");
         var currentPrice = GetFirstElementInnerText(analysisDoc, "//div[contains(@class, 'label yf-1i34qte')]");
         var defDoc = web.Load(defUrl);
-        var marketCap = GetFirstElementInnerText(defDoc, "//div[@class='container yf-i6syij']//p[@class='value yf-i6syij']");
-        return new Asset(ticker, decimal.Parse(currentPrice), decimal.Parse(futurePrice), marketCap);
+        var marketCap =
+            GetFirstElementInnerText(defDoc, "//div[@class='container yf-i6syij']//p[@class='value yf-i6syij']");
+        return new Asset(ticker, decimal.Parse(currentPrice), decimal.Parse(futurePrice), ConvertToDecimal(marketCap));
+    }
+
+    public Task<Asset> GetAssetAsync(string ticker)
+    {
+        return Task.Run(() => GetAsset(ticker));
     }
 
     public IEnumerable<Asset> GetAssets(IEnumerable<string> tickers)
     {
         return tickers.Select(GetAsset);
+    }
+
+    public Task<IEnumerable<Asset>> GetAssetsAssync(IEnumerable<string> tickers)
+    {
+        return Task.Run(() => GetAssets(tickers));
     }
 
     private static string GetFirstElementInnerText(HtmlDocument doc, string filter)
@@ -42,5 +52,33 @@ public class YahooHtmlPageReader : IAssetReader
             }
         }
         return string.Empty;
+    }
+
+    /// <summary>
+    /// Converts market capitalization from string to decimal.
+    /// </summary>
+    /// <param name="marketCap">String value, ex: 262.82B, 3.41T</param>
+    /// <returns>Decimal representation.</returns>
+    private decimal ConvertToDecimal(string marketCap)
+    {
+        char? lastChar = marketCap.LastOrDefault();
+        if (lastChar != null)
+        {
+            if (Descriptor.Constant.LargeNumberRepresentation.TryGetValue(lastChar.Value, out var multiplier))
+            {
+                if (decimal.TryParse(marketCap.Remove(marketCap.Length - 1), out var price))
+                {
+                    return price * multiplier;
+                }
+            }
+            else
+            {
+                if (decimal.TryParse(marketCap, out var price))
+                {
+                    return price;
+                }
+            }
+        }
+        return decimal.Zero;
     }
 }
